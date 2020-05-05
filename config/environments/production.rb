6b6332f0a6dfcc68c637176d53467695b7eef2f2
Casper Schuijt <casperschuijt@gmail.com>
2020-05-05 19:34:35 +0200

    credentials in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # encrypted credentials or something
+  config.require_master_key = true
 end
