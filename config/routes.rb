44a6b7f16d5a222fb32d37c69c107b3f4083ddd0
Casper Schuijt <casperschuijt@gmail.com>
2020-05-06 14:20:59 +0200

    resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'commissions/index'
-  get 'commissions/show'
-  get 'commissions/new'
-  get 'commissions/edit'
   #homepage
   root to: 'pages#home'
 
+  resources :commissions
 end
