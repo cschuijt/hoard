3ae86c993afc34868544bba91da36b3164e9a484
Casper Schuijt <casperschuijt@gmail.com>
2020-05-15 00:22:17 +0200

    more devise + omniauth config

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :twitter,
-            Rails.application.credentials.twitter[:key],
-            Rails.application.credentials.twitter[:secret],
-            authorize_params: { force_login: 'true' }
-end
