f62f6b7b198c796ffc412d5613794a4514b9a41b
Casper Schuijt <casperschuijt@gmail.com>
2020-05-15 13:04:45 +0200

    flashes in omniauth callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    sign_in_and_redirect @user
+    sign_in @user
+    flash[:success] = "You are now logged in as @" + @user.username
+    redirect_to root_url
   end
 
   def failure
+    flash[:danger] = "Authentication through Twitter failed."
     redirect_to root_url
   end
 end
