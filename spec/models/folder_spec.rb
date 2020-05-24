2c6e4421a4a0c45e95efae63abfc149e72490ec5
Casper Schuijt <casperschuijt@gmail.com>
2020-05-24 22:49:33 +0200

    folder model specs

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Folder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with valid data" do
+    it "should be valid" do
+      folder = build(:folder)
+      expect(folder).to be_valid
+    end
+  end
+
+  context "title" do
+    it "should not be nil" do
+      folder = build(:folder, title: nil)
+      expect(folder).to be_invalid
+    end
+
+    it "should not be empty" do
+      folder = build(:folder, title: "")
+      expect(folder).to be_invalid
+    end
+
+    it "should not be blank" do
+      folder = build(:folder, title: "      ")
+      expect(folder).to be_invalid
+    end
+
+    it "should not be too short" do
+      folder = build(:folder, title: "As")
+      expect(folder).to be_invalid
+    end
+
+    it "should not be too long" do
+      folder = build(:folder, title: "A"*200)
+      expect(folder).to be_invalid
+    end
+  end
+
+  context "user association" do
+    it "should not be missing" do
+      folder = build(:folder, user: nil)
+      expect(folder).to be_invalid
+    end
+  end
 end
