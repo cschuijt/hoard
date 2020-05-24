6b9d72aaf7e793ff6cfa8ccc5bf9e63954fb66be
Casper Schuijt <casperschuijt@gmail.com>
2020-05-25 01:55:29 +0200

    A few more request specs, authorization for folders

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,5 +1,7 @@
 class FoldersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+  before_action :private_filter,     only:   [:show]
+  before_action :correct_user,       except: [:index, :show, :new, :create]
 
   def index
     @folders = current_user.folders
@@ -45,5 +47,26 @@ class FoldersController < ApplicationController
   private
     def folder_params
       params.require(:folder).permit(:title, :description, :private)
+    end
+
+    def private_filter
+      @folder = Folder.find(params[:id])
+
+      if @folder.private?
+        authenticate_user!
+        
+        unless current_user == @folder.user
+          flash[:warning] = "This commission is marked private, only the uploader can access it."
+          redirect_to folders_url
+        end
+      end
+    end
+
+    def correct_user
+      @folder = current_user.folders.find_by(id: params[:id])
+      if @folder.nil?
+        flash[:warning] = "You are not authorized to perform this action."
+        redirect_to root_url
+      end
     end
 end
--- spec/requests/folders_request_spec.rb
+++ spec/requests/folders_request_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe "Folders", type: :request do
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
   end
 
-  describe "index page" do
+  describe "GET /folders" do
     context "when not logged in" do
       it "should redirect to the login page" do
         get "/folders"
 
-        expect(response).to redirect_to(:new_user_session)
+        expect(response).to redirect_to(new_user_session_url)
       end
     end
 
     context "when logged in" do
-      it "should be accessible" do
+      it "should be successful" do
         user = create(:user)
         log_in_as(user)
 
@@ -28,11 +28,49 @@ RSpec.describe "Folders", type: :request do
     end
   end
 
-  describe "show public folder" do
-    it "should be successful" do
-      folder = create(:folder)
-      get "/folders/#{folder.id}"
-      expect(response).to have_http_status(:success)
+  describe "GET /folders/:id" do
+    context "on a public folder" do
+      it "should be successful" do
+        folder = create(:folder)
+        get "/folders/#{folder.id}"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "on a private folder" do
+      context "when not logged in" do
+        it "should redirect to login page" do
+          folder = create(:folder, private: true)
+          get "/folders/#{folder.id}"
+
+          expect(response).to redirect_to(new_user_session_url)
+        end
+      end
+
+      context "when logged in as the correct user" do
+        it "should be successful" do
+          user = create(:user)
+          folder = create(:folder, user: user, private: true)
+
+          log_in_as(user)
+          get "/folders/#{folder.id}"
+
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "when logged in as another user" do
+        it "should redirect to folders index" do
+          user = create(:user)
+          folder = create(:folder, user: user, private: true)
+
+          other_user = create(:user)
+          log_in_as(other_user)
+          get "/folders/#{folder.id}"
+
+          expect(response).to redirect_to(folders_url)
+        end
+      end
     end
   end
 end
